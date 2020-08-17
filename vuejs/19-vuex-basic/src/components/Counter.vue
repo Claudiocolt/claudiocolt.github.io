3d9943f417586930f03f5e1f9735964b549aae32
Claudiocolt <xcoltx@gmail.com>
2020-08-17 13:28:41 -0400

    Vuex Basics

---
+++ vuejs/19-vuex-basic/src/components/Counter.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <p>{{ count }}</p>
+    <button @click="onClick()">Click Me!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  computed: {
+    count() {
+      return this.$store.state.count
+    }
+  },
+  methods: {
+    onClick() {
+      this.$store.commit('increment')
+    }
+  }
+}
+</script>
