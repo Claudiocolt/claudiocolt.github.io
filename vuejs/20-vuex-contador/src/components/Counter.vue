214b3d2ed4e6be583d712a6ae0f9832e5940de91
Claudiocolt <xcoltx@gmail.com>
2020-08-17 13:39:59 -0400

    [TAREA] contador-vuex

---
+++ vuejs/20-vuex-contador/src/components/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+
+    <h1>{{ title }}</h1>
+    <h5>Clicks: <span>{{ clicks }}</span></h5>
+    <button v-on:click="addCounter()">Click Counter</button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  props: {
+    title: String
+  },
+  computed: {
+    clicks() {
+      return this.$store.state.count
+    }
+  },
+  methods: {
+    addCounter() {
+      this.$store.commit('increment')
+    }
+  }
+}
+</script>
