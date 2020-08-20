c91ae8a19b511d068a60673ce108733f4b4c7349
Claudiocolt <xcoltx@gmail.com>
2020-08-20 19:33:16 -0400

    Vuex Getters

---
+++ vuejs/22-vuex-getters/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <MovieList :movies="familyMovies" genre="Comedy" />
+    <MovieList :movies="comedyMovies" genre="Family" />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex"
+import MovieList from './components/MovieList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MovieList
+  },
+  computed: {
+    ...mapGetters(["comedyMovies", "familyMovies"])
+  }
+}
+</script>
