39286411e3916a4dd2bf9d3225beb0aac36c2953
Claudiocolt <xcoltx@gmail.com>
2020-08-21 02:12:54 -0400

    Vuex Getters Avanzados

---
+++ vuejs/23-vuex-getters-avanzados/src/components/MovieList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>{{ genre }}</h2>
+    <ul>
+      <li v-for="movie in movies" :key="movie.id">{{ movie.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MovieList',
+  props: {
+    genre: String,
+    // movies: Array
+  },
+  computed: {
+    movies: function(){
+      return this.$store.getters.moviesByGenre(this.genre);
+    }
+  }
+}
+</script>
