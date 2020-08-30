ac255fc15af99573d0739a7ece038f0126885bb4
Claudiocolt <xcoltx@gmail.com>
2020-08-30 18:15:12 -0400

    Vue y Axios

---
+++ vuejs/29-axios-y-vue/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <p>The result of our request: {{ info }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data: function(){
+    return {
+      info: null
+    }
+  },
+  mounted: function(){
+    this.getBerryData();
+  },
+  methods: {
+    getBerryData: async function(){
+      try {
+        let response = await this.$http.get("https://pokeapi.co/api/v2/berry/42");
+        this.info = response.data;
+      } catch(error){
+        console.log(`Something went wrong: ${error}`);
+      }
+    }
+  }
+}
+</script>
