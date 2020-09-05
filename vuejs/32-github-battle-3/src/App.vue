9dd28d993efb12506eb4f3bbb33ca63326e0d6eb
Claudiocolt <xcoltx@gmail.com>
2020-09-05 19:54:52 -0400

    [TAREA] parte-tres

---
+++ vuejs/32-github-battle-3/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <Header :titulo="titulo"/>
+
+    <div class="container">
+      <GithubCard />
+      <GithubCard />
+    </div>
+    
+
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+import GithubCard from '@/components/GithubCard.vue'
+
+export default {
+  name: 'App',
+  data: function(){
+    return {
+      titulo: "Welcome to Github Battle (Aun Pendiente)",
+    }
+  },
+  components: {
+    Header,
+    GithubCard
+  }
+}
+</script>
+
+<style>
+html, body{
+  background-color: #fdfdfd;
+}
+.container{
+  display: flex;
+  justify-content: center;
+  text-align: center;
+}
+.container > div{
+  margin: 0 10px;
+}
+</style>
