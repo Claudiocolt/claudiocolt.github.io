655022a85562c2927a743873ec268c990eeb8ce5
Claudiocolt <xcoltx@gmail.com>
2020-09-06 20:03:15 -0300

    Vue + Cloud Firestore

---
+++ vuejs/33-my-vue-app/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    
+    <img class="logo" alt="Vue logo" src="./assets/logo.png">
+    <h1>Firebase Pets</h1>
+    <MyComponent />
+
+  </div>
+</template>
+
+<script>
+import MyComponent from './components/MyComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 30px;
+}
+.logo{
+  width: 100px;
+}
+h1{
+  margin: 0 0 20px 0;
+  font-size: 24px;
+}
+</style>
---
+++ vuejs/33-my-vue-app/src/components/MyComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="list-container">
+    <ul>
+      <li v-for="(pet, idx) in pets" :key="idx">{{ pet.pet_name }} ({{ pet.pet_age }} años)</li>
+    </ul>
+  </div>
+  
+</template>
+
+<script>
+import { db } from '@/firebase';
+export default {
+  name: "MyComponent",
+  data() {
+    return {
+      pets: []
+    }
+  },
+  firestore(){
+    return {
+      pets: db.collection('pets')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.list-container{
+  min-width: 100px;
+  margin: 0 auto;
+  display: flex;
+}
+ul{
+  margin: 0 auto;
+  padding: 0;
+  text-align: left;
+  list-style-position: inside;
+
+}
+ul li{
+  margin: 0;
+  padding: 0;
+}
+</style>
